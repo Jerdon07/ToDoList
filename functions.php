05d9323f4e898d2c9afea9fb7d5e2ae9a06dafc0
Jerdon07 <jdlitaoen@gmail.com>
2026-09-25 19:15:11 +0800

    FEAT: Add utility functions for debugging and URL matching

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+function dd(mixed $value): NULL 
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+    die();
+}
+
+function urlIs(mixed $uri): bool 
+{
+    return $_SERVER['REQUEST_URI'] === $uri;
+}
