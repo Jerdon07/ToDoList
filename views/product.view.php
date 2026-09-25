db948fd4994f0f3404ef848d9dd5bdf6aaf2741f
Jerdon07 <jdlitaoen@gmail.com>
2026-09-25 19:15:38 +0800

    FEAT: Refactor index and product view templates to use partials for improved structure

--- views/product.view.php
+++ views/product.view.php
@@ -1,48 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <title>Document</title>
-</head>
-<body>
-    <div class="min-h-full">
-        <nav class="bg-purple-700">
-            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div class="flex h-16 items-center justify-between">
-                    <!-- Logo Area -->
-                    <div>
-                        <h3 class="font-bold">Product App</h3>
-                    </div>
+<?php require 'partials/head.php' ?>
+<?php require 'partials/nav.php' ?>
+<?php require 'partials/banner.php' ?>
 
-                    <!-- Navigation Area -->
-                    <div class="flex items-baseline space-x-4">
-                        <a href="/" class="bg-purple-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">
-                            Home
-                        </a>
-
-                        <a href="/product.php" class="text-purple-200 hover:bg-purple-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                            Products
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-
-        <header class="bg-white shadow">
-            <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-light text-purple-900">
-                    Product
-                </h1>
-            </div>
-        </header>
-
-        <main>
-            <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                <p>Hello. Welcome to the Product page.</p>
-            </div>
-        </main>
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <p>Browse and Manage your products!</p>
     </div>
-</body>
-</html>
+</main>
+<?php require 'partials/footer.php' ?>
